d925b67ecc553f9c27aa0de49e2bc6db84e7b5ec
BRIAN MAKAU <brian.mutiso@student.moringaschool.com>
2021-09-15 08:39:54 +0300

    Add hover effect for portfolio

--- JS/scripts.js
+++ JS/scripts.js
@@ -16,5 +16,38 @@ $(document).ready(function(){
     $(".dev-logo").toggle();
     $(".dev-cont").toggle();
    });
+
+   //hover function for the portfolio images
+   $("#work4").hover(function(){
+    $(".grey").toggle();
+   });
+
+   $("#work3").hover(function(){
+    $(".ontario").toggle();
+   });
+
+   $("#work2").hover(function(){
+    $(".orange").toggle();
+   });
+
+   $("#work1").hover(function(){
+    $(".jim").toggle();
+   });
+
+   $("#work5").hover(function(){
+    $(".health").toggle();
+   });
+
+   $("#work6").hover(function(){
+    $(".mac").toggle();
+   });
+
+   $("#work7").hover(function(){
+    $(".burned").toggle();
+   });
+
+   $("#work8").hover(function(){
+    $(".giraffe").toggle();
+   });
 });
 
